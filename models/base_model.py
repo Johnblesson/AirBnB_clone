9704964c7b4555739e87517233fc578f6e6d8f73
james-eo <108140375+james-eo@users.noreply.github.com>
2023-03-13 04:52:56 +0100

    Add files via upload
    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,58 +1,34 @@
 #!/usr/bin/python3
 
-"""
-    Defines a class BaseModel.
-"""
-
+import uuid
 from datetime import datetime
-from uuid import uuid4
-import models
 
-
-class BaseModel:
-    """Represent a BaseModel."""
+class BaseModel():
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel"""
 
-        if not kwargs:
-            self.id = str(uuid4())
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "updated_at" or key == "created_at":
+                    self.__dict__[key] = datetime.fromisoformat(value)
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
-            for key, val in kwargs.items():
-                if key != '__class__':
-                    if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.strptime(
-                            val, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, val)
 
     def __str__(self):
-        """Return a string representation of the class"""
-
-        return "[{:s}] ({:s}) {:s}".format(
-            self.__class__.__name__,
-            self.id,
-            str(self.__dict__)
-        )
-
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
     def save(self):
-        """Update the date field"""
-
         self.updated_at = datetime.now()
-        models.storage.save()
-
+    
     def to_dict(self):
-        """Return a dictionary representation of the instance"""
-
-        rep = {
-            "__class__": self.__class__.__name__,
-            "created_at": self.created_at.isoformat(),
-            "updated_at": self.updated_at.isoformat()
-        }
-
         dict_copy = self.__dict__.copy()
-        dict_copy.update(rep)
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
         return dict_copy
